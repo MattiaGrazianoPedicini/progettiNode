eabc5341423349ee90ae82bbc935797b57c94a49
MattiaGrazianoPedicini <pedicinimattiagraziano@itis-molinari.eu>
2024-01-10 18:14:34 +0100

    completamento es 3

--- Pedicini_Es3/es3.js
+++ Pedicini_Es3/es3.js
@@ -1,17 +1,29 @@
-const fs= require(`fs`);
-fs.readFile("660000_parole_italiane.txt", (error, data) =>{
-if (error) {
-throw error;
-}
-console.log(data.toString());
+const fs = require(`fs`);
+let vocabolario = [];
+fs.readFile("660000_parole_italiane.txt", (error, data) => {
+  if (error) {
+    throw error;
+  }
+  vocabolario = data.toString().split("\n");
 });
 
-const readline = require(`readline`).createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+function cercaRima(listaParole, parola) {
+  const risultato = [];
+  const rima = parola.substring(parola.length - 3);
+  for (let i = 0; i < listaParole.length; i++) {
+    if (listaParole[i].substring(listaParole[i].length - 3) === rima) {
+      risultato.push(listaParole[i]);
+    }
+  }
+  return risultato;
+}
 
-readline.question(`Inserire numero: `, (n) => {
-    
-    readline.close();
-  });
+const readline = require(`readline`).createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+readline.question(`Inserire parola: `, (n) => {
+  console.log(cercaRima(vocabolario, n));
+  readline.close();
+});
